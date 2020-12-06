3e0e5e7f7e19dc9335cc73d8d19c12659bb7d5cc
Ihar Bulaty <Ihar_Bulaty@epam.com>
2020-12-06 23:05:41 +0300

    Task_8_add_cart_api_path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   products: 'https://4fzkh684g8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://cqa4tyx9yh.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://4fzkh684g8.execute-api.eu-west-1.amazonaws.com/dev/products'
+  bff: 'https://4fzkh684g8.execute-api.eu-west-1.amazonaws.com/dev/products',
+  cart: 'http://cart-api-dev-2.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
